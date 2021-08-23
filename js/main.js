b2053756824cf3371af78f8bc899e7b535a5f1d2
kiwi <85372400+bonobono-kiwi@users.noreply.github.com>
2021-08-23 23:45:54 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@
   const prev = document.getElementById('prev');
   const wrap = document.getElementById('items-contents-wrap');
   const items = document.getElementById('items');
+  const item1 = document.getElementById('item1-bg');
+  const item2 = document.getElementById('item2-bg');
+  const item3 = document.getElementById('item3-bg');
   const item1Image = document.getElementById('item1');
   const item2Image = document.getElementById('item2');
   const item3Image = document.getElementById('item3');
@@ -13,13 +16,48 @@
   let currentIndex = 0;
 
   var itemsTopPosition = items.getBoundingClientRect().top;
+
+  function show1(){
+    item1Image.style.opacity = 1;
+    item1Content.style.opacity = 1;
+  }
+  function show2(){
+    item2Image.style.opacity = 1;
+    item2Content.style.opacity = 1;
+  }
+  function show3(){
+    item3Image.style.opacity = 1;
+    item3Content.style.opacity = 1;
+  }
+
   
-  window.addEventListener('scroll', ()=>{
-    if(window.pageYOffset+650 > itemsTopPosition){
-      item1Image.style.opacity = 1;
-      item1Content.style.opacity = 1;
-    }
-  });
+  function scrollSmartPhone(){
+    window.addEventListener('scroll', ()=>{
+      if(window.pageYOffset+650 > itemsTopPosition){
+        item1Image.style.opacity = 1;
+        item1Content.style.opacity = 1;
+        item2Image.style.opacity = 1;
+        item2Content.style.opacity = 1;
+        item3Image.style.opacity = 1;
+        item3Content.style.opacity = 1;
+      }
+    });
+  }
+
+  function scrollPc(){
+    window.addEventListener('scroll', ()=>{
+      if(window.pageYOffset+550 > itemsTopPosition){
+        setTimeout(show1, 500);
+        setTimeout(show2, 600);
+        setTimeout(show3, 700);
+        
+        item1.style.left = 0;
+        item2.style.left = 0;
+        item3.style.left = 0;
+      }
+    });
+  }
+
 
 
 
@@ -28,6 +66,9 @@
   var windowWidth = window.innerWidth;
   //スマホ画面
   if(windowWidth < 1025){
+
+    scrollSmartPhone();
+
     next.addEventListener('click', ()=>{
       currentIndex++;
       wrap.style.transform = `translateX(${-100 * currentIndex}vw)`;
@@ -81,7 +122,11 @@
     });
   }else{
 
-  }
+    scrollPc();
 
+
+
+  }
+  
 
 }
