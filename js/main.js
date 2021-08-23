7cff6115f36fd6d9573037701d050b55446bad30
kiwi <85372400+bonobono-kiwi@users.noreply.github.com>
2021-08-24 01:05:14 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 'use strict';
 {
+  const header = document.getElementById('header');
+  const concept = document.getElementById('concept');
+  const news = document.getElementById('news');
   const next = document.getElementById('next');
   const prev = document.getElementById('prev');
   const wrap = document.getElementById('items-contents-wrap');
   const items = document.getElementById('items');
+  const shopInfo = document.getElementById('shop-info');
+  const footer = document.getElementById('footer');
   const item1 = document.getElementById('item1-bg');
   const item2 = document.getElementById('item2-bg');
   const item3 = document.getElementById('item3-bg');
@@ -16,6 +21,8 @@
   let currentIndex = 0;
 
   var itemsTopPosition = items.getBoundingClientRect().top;
+  var shopInfoTopPosition = shopInfo.getBoundingClientRect().top;
+  var footerTopPosition = footer.getBoundingClientRect().top;
 
   function show1(){
     item1Image.style.opacity = 1;
@@ -40,6 +47,13 @@
         item2Content.style.opacity = 1;
         item3Image.style.opacity = 1;
         item3Content.style.opacity = 1;
+        items.classList.add('show');
+      }
+      if(window.pageYOffset +500> shopInfoTopPosition){
+        shopInfo.classList.add('show');
+      }
+      if(window.pageYOffset +500 > footerTopPosition){
+        footer.classList.add('show');
       }
     });
   }
@@ -69,6 +83,7 @@
 
     scrollSmartPhone();
 
+    //カルーセルここから
     next.addEventListener('click', ()=>{
       currentIndex++;
       wrap.style.transform = `translateX(${-100 * currentIndex}vw)`;
@@ -120,6 +135,27 @@
         item3Content.style.opacity = 1;  
       }
     });
+    //カルーセルここまで
+
+    //ロード後ふわっと現れるアニメーション
+    function headerShow(){
+      header.classList.add('show');
+    }
+    function conceptShow(){
+      concept.classList.add('show');
+    }
+    function newsShow(){
+      news.classList.add('show');
+    }
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+      setTimeout(headerShow,400);
+      setTimeout(conceptShow,600);
+      setTimeout(newsShow,800);
+   });
+
+
   }else{
 
     scrollPc();
